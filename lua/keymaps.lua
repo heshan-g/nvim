9c0e459bcddd58e2e13949f93b2e18af85349b36
Heshan <heshan.ua@gmail.com>
2023-06-24 02:08:31 +0530

    Add keymap to enter normal mode in terminal

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,11 +14,6 @@ return {
   vim.keymap.set("n", "<leader>x", ":bd<CR>", { silent = true }),
   vim.keymap.set("n", "<leader>X", ":bd!<CR>", { silent = true }),
 
-  -- -- floating terminal
-  -- local lazyterm = function() Util.float_term(nil, { cwd = Util.get_root() }) end
-  -- vim.keymap.set("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
-  -- vim.keymap.set("n", "<leader>fT", function() Util.float_term() end, { desc = "Terminal (cwd)" })
-  -- vim.keymap.set("n", "<c-/>", lazyterm, { desc = "Terminal (root dir)" })
-  -- vim.keymap.set("n", "<c-_>", lazyterm, { desc = "which_key_ignore" })
+  -- terminal
+  vim.keymap.set("t", "<esc><esc>", '<C-\\><C-n>')
 }
-
