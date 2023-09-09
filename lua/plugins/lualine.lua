25fe9721d06b7ac2f0bfd74b79d6ed881d254dc8
Heshan <heshan.ua@gmail.com>
2023-09-09 12:08:59 +0530

    Update theme to Kanagawa

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      theme = 'tokyonight',
+      theme = 'kanagawa',
       component_separators = '|',
       section_separators = '',
     },
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,9 @@
 return {
-  'folke/tokyonight.nvim',
+  'rebelot/kanagawa.nvim',
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
-    vim.cmd.colorscheme 'tokyonight'
+    vim.cmd.colorscheme 'kanagawa'
   end,
 }
