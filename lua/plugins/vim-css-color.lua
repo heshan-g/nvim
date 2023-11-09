67b94bbea82a59629bedf80995fb747c2c6b7425
Heshan <heshan.ua@gmail.com>
2023-11-09 21:41:23 +0530

    Add vim-css-color

---
+++ lua/plugins/vim-css-color.lua
@@ -0,0 +1,16 @@
+return {
+  "ap/vim-css-color",
+  version = "*",
+  event = "VeryLazy",
+  config = function()
+    -- require("FTerm").setup({
+    --   border = "rounded",
+    --   dimensions  = {
+    --     height = 0.95,
+    --     width = 0.8,
+    --   },
+    --   -- blend = 15,
+    --   auto_close = true,
+    -- })
+  end
+}
