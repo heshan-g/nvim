2d7818172c020cea33986888d29cfad944036fb6
Heshan <heshan.ua@gmail.com>
2024-02-26 23:10:58 +0530

    Add visual mode to signature help

--- init.lua
+++ init.lua
@@ -391,7 +391,7 @@ local on_attach = function(_, bufnr)
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  vim.keymap.set({ 'n', 'i' }, '<C-k>', vim.lsp.buf.signature_help)
+  vim.keymap.set({ 'n', 'i', 'v' }, '<C-k>', vim.lsp.buf.signature_help)
 
   -- Lesser used LSP functionality
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
