859c35df57dafdde7c21d7458ed13e7765ea312c
Heshan <heshan.ua@gmail.com>
2024-03-20 07:47:37 +0530

    Add buffer navigation keymaps

--- lua/config/keymaps/buffers.lua
+++ lua/config/keymaps/buffers.lua
@@ -1,4 +1,6 @@
 return {
-  -- Open a blank buffer when closing the last buffer
-  vim.keymap.set('n', '<leader>q', '<cmd>bp<bar>sp<bar>bn<bar>bd<CR>', { desc = 'Close buffer' })
+    -- Open a blank buffer when closing the last buffer
+    vim.keymap.set('n', '<leader>q', '<cmd>bp<bar>sp<bar>bn<bar>bd<CR>', { desc = 'Close buffer' }),
+    vim.keymap.set('n', 'H', '<cmd>bp<CR>', { desc = 'Previous buffer' }),
+    vim.keymap.set('n', 'L', '<cmd>bn<CR>', { desc = 'Next buffer' }),
 }
