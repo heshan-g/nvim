fd79a1a1a2c96f7241ca8ca83206c061aade8642
Heshan <heshan.uag@gmail.com>
2025-11-27 21:10:28 +1300

    Udate neotree - replace rm with trash when deleting

--- lua/config/plugins/neotree.lua
+++ lua/config/plugins/neotree.lua
@@ -11,6 +11,14 @@ return {
   config = function()
     require('neo-tree').setup {
       filesystem = {
+        commands = {
+          -- Override delete to use trash instead of rm
+          delete = function(state)
+            local path = state.tree:get_node().path
+            vim.fn.system({ "trash", vim.fn.fnameescape(path) })
+            require("neo-tree.sources.manager").refresh(state.name)
+          end,
+        },
         filtered_items = {
           visible = true,
           hide_dotfiles = false,
