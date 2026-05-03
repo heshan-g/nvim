7f1d70ba6395906ddd974d089a4e333316410c43
Heshan <heshan.uag@gmail.com>
2026-05-03 23:43:53 +1200

    Add lsp configs

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 require("config")
 require("config.lazy")
 require("config.keymaps")
+
+-- LSPs ------------------------------------------------------------------------
+vim.lsp.config("luals", require("config.lsp.luals"))
+vim.lsp.enable("luals")
+
+vim.lsp.config("tsserver", require("config.lsp.tsserver"))
+vim.lsp.enable("tsserver")
+
